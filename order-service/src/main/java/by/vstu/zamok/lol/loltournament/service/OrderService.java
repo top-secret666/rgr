b9f78f62d368b59b13ec883238045bdba38b0cb9
top-secret666 <chupacabra2.0666@gmail.com>
2025-11-01 10:00:00 +0000

    refactor: order-service

---
+++ order-service/src/main/java/by/vstu/zamok/lol/loltournament/service/OrderService.java
@@ -0,0 +1,26 @@
+package by.vstu.zamok.lol.loltournament.service;
+
+import by.vstu.zamok.lol.loltournament.entity.Order;
+import by.vstu.zamok.lol.loltournament.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+    private final OrderRepository orderRepository;
+
+    public Order createOrder(Order order) {
+        return orderRepository.save(order);
+    }
+
+    public List<Order> getAllOrders() {
+        return orderRepository.findAll();
+    }
+
+    public Order getOrderById(Long id) {
+        return orderRepository.findById(id).orElse(null);
+    }
+}
