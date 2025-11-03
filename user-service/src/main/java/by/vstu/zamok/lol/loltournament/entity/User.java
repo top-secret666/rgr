37b81b5ab579ceb93ca2db09f729b9d41e61034d
top-secret666 <chupacabra2.0666@gmail.com>
2025-11-03 22:00:00 +0300

    feat: add user CRUD

--- user-service/src/main/java/by/vstu/zamok/lol/loltournament/entity/User.java
+++ user-service/src/main/java/by/vstu/zamok/lol/loltournament/entity/User.java
@@ -1,52 +1,20 @@
 package by.vstu.zamok.lol.loltournament.entity;
 
-import jakarta.persistence.*;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
-import lombok.*;
-
-import java.util.HashSet;
-import java.util.Set;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
 
 @Entity
-@Table(name = "users",
-        uniqueConstraints = {
-                @UniqueConstraint(columnNames = "username"),
-                @UniqueConstraint(columnNames = "email")
-        })
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
+@Table(name = "users")
+@Data
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @NotBlank
-    @Size(max = 20)
     private String username;
-
-    @NotBlank
-    @Size(max = 50)
-    @Email
-    private String email;
-
-    @NotBlank
-    @Size(max = 120)
     private String password;
-
-    @Enumerated(EnumType.STRING)
-    @Column(name = "role")
-    private ERole role;
-
-    @OneToMany(mappedBy = "creator", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    private Set<Tournament> createdTournaments = new HashSet<>();
-
-    public User(String username, String email, String password) {
-        this.username = username;
-        this.email = email;
-        this.password = password;
-    }
+    private String email;
 }
---
+++ user-service/src/main/java/by/vstu/zamok/lol/loltournament/service/UserService.java
@@ -0,0 +1,30 @@
+package by.vstu.zamok.lol.loltournament.service;
+
+import by.vstu.zamok.lol.loltournament.entity.User;
+import by.vstu.zamok.lol.loltournament.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User findById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteById(Long id) {
+        userRepository.deleteById(id);
+    }
+}
