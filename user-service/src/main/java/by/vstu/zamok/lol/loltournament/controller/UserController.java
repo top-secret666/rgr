a54a781467118de372489f5f38e8b9bf4490635c
top-secret666 <chupacabra2.0666@gmail.com>
2025-11-03 22:00:00 +0300

    feat: add user CRUD

---
+++ user-service/src/main/java/by/vstu/zamok/lol/loltournament/controller/UserController.java
@@ -0,0 +1,41 @@
+package by.vstu.zamok.lol.loltournament.controller;
+
+import by.vstu.zamok.lol.loltournament.entity.User;
+import by.vstu.zamok.lol.loltournament.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+@AllArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping
+    public List<User> getAll() {
+        return userService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public User getById(@PathVariable Long id) {
+        return userService.findById(id);
+    }
+
+    @PostMapping
+    public User create(@RequestBody User user) {
+        return userService.save(user);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        userService.deleteById(id);
+    }
+}
