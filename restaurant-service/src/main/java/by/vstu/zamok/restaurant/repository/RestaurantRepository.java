634f6b52f8c5e270b72fd0852cc5abdc1f3c5bfc
top-secret666 <chupacabra2.0666@gmail.com>
2025-11-05 12:00:00 +0300

    feat: Scaffold entities and repositories for user and restaurant services

--- restaurant-service/src/main/java/by/vstu/zamok/restaurant/repository/RestaurantRepository.java
+++ restaurant-service/src/main/java/by/vstu/zamok/restaurant/repository/RestaurantRepository.java
@@ -1,7 +1,10 @@
-package by.vstu.zamok.restaurant.repository;
-
 import by.vstu.zamok.restaurant.entity.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
+    List<Restaurant> findAllByNameContainingIgnoreCase(String name);
 }
--- restaurant-service/src/main/java/by/vstu/zamok/restaurant/service/RestaurantService.java
+++ restaurant-service/src/main/java/by/vstu/zamok/restaurant/service/RestaurantService.java
@@ -1,30 +1,39 @@
 package by.vstu.zamok.restaurant.service;
 
+import by.vstu.zamok.restaurant.dto.RestaurantDto;
 import by.vstu.zamok.restaurant.entity.Restaurant;
+import by.vstu.zamok.restaurant.mapper.RestaurantMapper;
 import by.vstu.zamok.restaurant.repository.RestaurantRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 
-@Service
+
+
+
 @AllArgsConstructor
 public class RestaurantService {
     private final RestaurantRepository restaurantRepository;
+    private final RestaurantMapper restaurantMapper;
 
-    public List<Restaurant> findAll() {
-        return restaurantRepository.findAll();
+    public List<RestaurantDto> findAll() {
+        return restaurantMapper.toDto(restaurantRepository.findAll());
     }
 
-    public Restaurant findById(Long id) {
-        return restaurantRepository.findById(id).orElse(null);
+    public RestaurantDto findById(Long id) {
+        return restaurantMapper.toDto(restaurantRepository.findById(id).orElse(null));
     }
 
-    public Restaurant save(Restaurant restaurant) {
-        return restaurantRepository.save(restaurant);
+    public RestaurantDto save(RestaurantDto restaurant) {
+        Restaurant entity = restaurantMapper.toEntity(restaurant);
+        return restaurantMapper.toDto(restaurantRepository.save(entity));
     }
 
     public void deleteById(Long id) {
         restaurantRepository.deleteById(id);
+    }
+
+    public List<RestaurantDto> findByName(String name) {
+        return restaurantMapper.toDto(restaurantRepository.findAllByNameContainingIgnoreCase(name));
     }
 }
