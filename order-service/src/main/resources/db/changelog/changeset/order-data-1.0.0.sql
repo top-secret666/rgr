864111681d33fca7a4ea3b701f91aac4bf6d6e21
top-secret666 <chupacabra2.0666@gmail.com>
2025-11-07 02:30:00 +0000

    fix: database orders

--- order-service/src/main/resources/db/changelog/changeset/order-data-1.0.0.sql
+++ order-service/src/main/resources/db/changelog/changeset/order-data-1.0.0.sql
@@ -1,12 +1,14 @@
+--liquibase formatted sql
+--changeset zham:5
 
--- Вставляем один тестовый заказ
-INSERT INTO "order" (id, user_id, restaurant_id, status, special_requests, created_at, updated_at)
-VALUES (1, 101, 201, 'PENDING', 'Пожалуйста, без лука', NOW(), NOW());
+-- Вставляем один тестовый заказ. Total price = (2 * 250) + (1 * 500) = 1000
+INSERT INTO order_schema.orders (id, user_id, restaurant_id, status, order_date, total_price)
+VALUES (1, 101, 201, 'PENDING', NOW(), 1000);
 
 -- Вставляем две позиции для этого заказа
-INSERT INTO order_item (order_id, dish_id, quantity, price)
-VALUES (1, 301, 2, 250.75),
-       (1, 302, 1, 500.00);
+INSERT INTO order_schema.order_item (order_id, dish_id, quantity, price)
+VALUES (1, 301, 2, 250),
+       (1, 302, 1, 500);
 
 -- Обновляем sequence, чтобы новые заказы начинались с ID 2
-ALTER SEQUENCE order_id_seq RESTART WITH 2;
+ALTER SEQUENCE order_schema.orders_id_seq RESTART WITH 2;
