38896f3b853c52b2e8f5a89b78bfe2c819d24c25
FBI <chupacabra2.0666@gmail.com>
2025-12-29 09:00:00 +0300

    fix(user): hide password field from responses via write-only JSON property

--- user-service/src/main/java/by/vstu/zamok/user/dto/UserDto.java
+++ user-service/src/main/java/by/vstu/zamok/user/dto/UserDto.java
@@ -1,5 +1,9 @@
 package by.vstu.zamok.user.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.util.Set;
@@ -7,8 +11,19 @@ import java.util.Set;
 @Data
 public class UserDto {
     private Long id;
+
+    @NotBlank(message = "email must not be blank")
+    @Email(message = "email must be valid")
     private String email;
+
+    @Size(min = 3, max = 255, message = "fullName length must be between 3 and 255")
     private String fullName;
+
+    @NotBlank(message = "password must not be blank")
+    @Size(min = 6, max = 255, message = "password length must be at least 6")
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+
     private Set<AddressDto> addresses;
     private Set<String> roles;
 }
